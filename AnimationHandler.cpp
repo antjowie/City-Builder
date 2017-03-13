6580538becba934e69c6dea0961823d052e34ea1
Angelo Anthony Rettob <angelo05rettob@gmail.com>
2017-03-13 22:48:25 +0100

    Made functions for animation handler class

--- AnimationHandler.cpp
+++ AnimationHandler.cpp
@@ -1,4 +1,5 @@
 #include "AnimationHandler.h"
+#include <math.h>
 
 unsigned int Animation::GetLength() const
 {
@@ -10,4 +11,55 @@ Animation::Animation(const unsigned int startFrame, const unsigned int endFrame,
 	endFrame(endFrame),
 	duration(duration)
 {
+}
+
+void AnimationHandler::AddAnim(Animation & anim)
+{
+	animations.push_back(anim);
+}
+
+void AnimationHandler::ChangeAnim(unsigned int animNum)
+{
+	if (currentAnim == animNum || animNum >= animations.size() || animNum < 0) return;
+
+	currentAnim = animNum;
+
+	sf::IntRect rect = frameSize;
+	rect.top = rect.height * animNum;
+	bounds = rect;
+	elapsed = fmod(elapsed, animations.at(animNum).duration);
+}
+
+void AnimationHandler::Update(const float dt)
+{
+	if (currentAnim >= animations.size() || currentAnim < 0) return;
+
+	const float duration = animations.at(currentAnim).duration;
+
+	if (int(elapsed + dt) > int(duration)) {
+		int frame = (elapsed + dt) / duration;
+
+		frame %= animations.at(currentAnim).GetLength();
+
+		sf::IntRect rect = frameSize;
+		rect.left = rect.width* frame;
+		rect.top = rect.height* currentAnim;
+		bounds = rect;
+	}
+
+	elapsed += dt;
+	if (elapsed > duration* animations.at(currentAnim).GetLength())
+		elapsed = fmod(elapsed,duration*animations.at(currentAnim).GetLength());
+}
+
+AnimationHandler::AnimationHandler():
+	elapsed(0.f),
+	currentAnim(-1)
+{
+}
+
+AnimationHandler::AnimationHandler(const sf::IntRect & frameSize):
+	AnimationHandler()
+{
+	this->frameSize = frameSize;
 }
--- AnimationHandler.h
+++ AnimationHandler.h
@@ -1,10 +1,7 @@
 #pragma once
-class AnimationHandler
-{
-private:
 
-public:
-};
+#include "SFML\Graphics.hpp"
+#include <vector>
 
 struct Animation
 {
@@ -16,4 +13,24 @@ struct Animation
 	unsigned int GetLength() const;
 
 	Animation(const unsigned int startFrame, const unsigned int endFrame, const float duration);
+};
+
+class AnimationHandler
+{
+private:
+	std::vector<Animation> animations;
+
+	float elapsed;
+
+public:
+	// Selection of the texture that should be displayed
+	sf::IntRect bounds;
+	sf::IntRect frameSize;
+
+	void AddAnim(Animation& anim);
+	void ChangeAnim(unsigned int animNum);
+	void Update(const float dt);
+
+	AnimationHandler();
+	AnimationHandler(const sf::IntRect& frameSize);
 };
